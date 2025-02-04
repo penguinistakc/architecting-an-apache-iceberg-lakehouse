cddc2fa735c36708b05d07db32427ac24d9e4d7f
Alex Merced <alex@alexmerced.dev>
2025-02-04 17:12:28 -0500

    Create example.py

---
+++ ch2/example.py
@@ -0,0 +1,34 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+## DEFINE VARIABLES
+CATALOG_URI = "http://nessie:19120/api/v1"
+WAREHOUSE = "s3://warehouse/"
+STORAGE_URI = "http://minio:9000"
+
+## CONFIGURE SPARK SESSION
+conf = (
+    pyspark.SparkConf()
+        .setAppName('Iceberg Ingestion')
+        .set('spark.jars.packages', 
+             'org.postgresql:postgresql:42.7.3,'
+             'org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.0,'
+             'org.projectnessie.nessie-integrations:nessie-spark-extensions-3.5_2.12:0.77.1,'
+             'software.amazon.awssdk:bundle:2.24.8,'
+             'software.amazon.awssdk:url-connection-client:2.24.8')
+        .set('spark.sql.extensions', 
+             'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,'
+             'org.projectnessie.spark.extensions.NessieSparkSessionExtensions')
+        .set('spark.sql.catalog.nessie', 'org.apache.iceberg.spark.SparkCatalog')
+        .set('spark.sql.catalog.nessie.uri', CATALOG_URI)
+        .set('spark.sql.catalog.nessie.ref', 'main')
+        .set('spark.sql.catalog.nessie.authentication.type', 'NONE')
+        .set('spark.sql.catalog.nessie.catalog-impl', 'org.apache.iceberg.nessie.NessieCatalog')
+        .set('spark.sql.catalog.nessie.s3.endpoint', STORAGE_URI)
+        .set('spark.sql.catalog.nessie.warehouse', WAREHOUSE)
+        .set('spark.sql.catalog.nessie.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO')
+)
+
+## START SPARK SESSION
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+print("Spark Running")
